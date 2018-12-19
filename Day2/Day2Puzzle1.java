b7878707a00430c572054a62d49345a21f3e14da
OZ <oxz3@psu.edu>
2018-12-18 19:29:26 -0800

    Day2 Puzzle1 solved.  Enter 0 as input to finish input and generate checksum.

--- Day2/Day2Puzzle1.java
+++ Day2/Day2Puzzle1.java
@@ -7,22 +7,37 @@ public class Day2Puzzle1
     public static void main (String args[])
     {
         String input;
+        int twice = 0;
+        int thrice = 0;
 
         Scanner keyboard = new Scanner(System.in);
         System.out.println(">");
 
         do {
             input = keyboard.next();
-            HashMap inputHashMap = new HashMap (input.length());
+            HashMap<Character,Integer> inputHashMap = new HashMap<Character,Integer> (input.length());
 
             for (int i = 0; i < input.length(); i++)
             {
-                System.out.println(input.charAt(i));
+
+                if (inputHashMap.containsKey(input.charAt(i))) {
+                    int currentValue = inputHashMap.get(input.charAt(i));
+                    inputHashMap.put(input.charAt(i), currentValue + 1);
+                }
+
+                else {
+                    inputHashMap.put(input.charAt(i), 1);
+                }
             }
 
-            inputHashMap
+            if (inputHashMap.containsValue(2))
+                twice++;
+            if (inputHashMap.containsValue(3))
+                thrice++;
+
         } while (!input.equals("0"));
 
-
+        int checksum = twice * thrice;
+        System.out.println("Checksum is " + checksum);
     }
 }
