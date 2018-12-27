c1ae35fe66582108358cbb1b58e5b54416948bdb
Oliver <oxz3@psu.edu>
2018-12-27 15:20:47 -0800

    Day6WIP - Need to figure out how to correctly identify the four outer bounds with infinite spread.

--- Day6/Day6Puzzle1.java
+++ Day6/Day6Puzzle1.java
@@ -3,14 +3,61 @@ import java.io.*;
 import java.util.*;
 
 public class Day6Puzzle1 {
-    public static void main (String args[]) {
+    public static void main (String args[]) throws Exception {
         File file = new File ("sample.txt");
         Scanner fileInput = new Scanner (file);
-        String origInput = "";
-        int shortestPolymer = 10000;
+        String input = "";
+        ArrayList<String> inputAL = new ArrayList<>();
 
         while (fileInput.hasNextLine()) {
-            origInput = fileInput.nextLine();
-            System.out.println(origInput);
+            input = fileInput.nextLine();
+            inputAL.add(input);
+        }
+
+        //Parse out string into ints
+        //Find the 4 corners of the output
+        //Place values into a 2-dim array of ints
+        //Use nested loops to evaluate distance and populate array
+        //Loop again through array to count the highest occurrences, that is not one of the 4 corners
+
+        ArrayList<Integer> xCoords = new ArrayList<>();
+        ArrayList<Integer> yCoords = new ArrayList<>();
+
+        for (String coords : inputAL) {
+            String[] xyCoords = coords.split(",");
+            xyCoords[1] = xyCoords[1].trim();
+            xCoords.add(Integer.parseInt(xyCoords[0]));
+            yCoords.add(Integer.parseInt(xyCoords[1]));
+        }
+
+
+        int biggestX = 0;
+        int biggestY = 0;
+        int smallestX = 1000;
+        int smallestY = 1000;
+        for (int xValue : xCoords) {
+            if (biggestX < xValue)
+                biggestX = xValue;
+            if (smallestX > xValue)
+                smallestX = xValue;
+        }
+        for (int yValue : yCoords) {
+            if (biggestY < yValue)
+                biggestY = yValue;
+            if (smallestY > yValue)
+                smallestY = yValue;
+        }
+
+        System.out.println(smallestX + ", " + yCoords.get(xCoords.indexOf(smallestX)));
+        System.out.println(xCoords.get(yCoords.indexOf(smallestY)) + ", " + smallestY);
+        System.out.println(biggestX + ", " + yCoords.get(xCoords.indexOf(biggestX)));
+        System.out.println(xCoords.get(yCoords.indexOf(biggestY)) + ", " + biggestY);
+
+
+        int[][] grid = new int[biggestX + 1][biggestY + 1];
+        for (int i = 0; i < xCoords.size(); i++) {
+            grid[xCoords.get(i)][yCoords.get(i)] = i;
+        }
+
     }
 }
